c5b8e127146694cb2e228a565d7281bcc416b92e
BarbaraC <55756205+BarbaraC12@users.noreply.github.com>
2023-07-16 20:38:43 +0200

    Add ft_strlen.s

---
+++ ft_strlen.s
@@ -0,0 +1,22 @@
+global ft_strlen
+
+ft_strlen:
+	xor		rcx, rcx				; on copie 0 dans rcx
+ 	# xor		rax, 0					; on met rax a 0 
+
+loop:
+	cmp		BYTE [rdi + rcx], 0		;
+  	jz		return					; si jz = (rdi[rcx] == 0) -> return
+	inc		rcx						; sinon on incremente rcx
+ 	# cmp		BYTE [rdi + rax], 0		;
+	# je		return					; si je = (rdi[rax] == 0) -> return
+ 	# inc		rax						; sinon on incremente rcx
+	jmp		loop					; boucle sur loop
+
+return:
+	mov 		rax, rcx			; on copie rcx dans rax
+	ret								; return rax
+
+# -------------------------------------------------------------- #
+# Use rcx is a good practice ? or is worth to use rax directly ? #
+# -------------------------------------------------------------- #
